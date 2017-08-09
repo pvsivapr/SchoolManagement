1f9dd335b3e9d9035238a477abc5d4156e37311f
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2017-08-09 15:33:11 +0530

    Committing Home Page and respective Images

---
+++ MEDU/MEDU/Views/Student/HomeStudent.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MEDU
+{
+    public partial class HomeStudent : ContentPage
+    {
+        public HomeStudent()
+        {
+            InitializeComponent();
+        }
+
+        void ProfileNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModalAsync(new );
+        }
+        void DairyNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModnew );
+        }
+        void ExamsNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModnew );
+        }
+        void EventsNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModnew );
+        }
+        void HolidaysNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModnew );
+        }
+        void FeesNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigationnew );
+        }
+        void AboutUsNavigationClicked(object sender, EventArgs e)
+        {
+            //Navigationnew );
+        }
+
+    }
+}
