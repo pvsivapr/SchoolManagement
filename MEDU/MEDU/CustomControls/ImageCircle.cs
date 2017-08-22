9270941e5012673d554c5bcd906f3b54ce8a0f0a
DevRabbit <devrabbit@DevRabbits-Mac-mini.local>
2017-08-22 10:23:55 +0530

    Committing ImageCircle

---
+++ MEDU/MEDU/CustomControls/ImageCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MEDU
+{
+	public class ImageCircle : Image
+	{
+		public ImageCircle()
+		{
+
+		}
+	}
+}
---
+++ MEDU/iOS/CustomControls/ImageCircleRender.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using MEDU;
+using MEDU.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(ImageCircle), typeof(ImageCircleRenderers))]
+
+namespace MEDU.iOS
+{
+	public class ImageCircleRenderers : ImageRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null || Element == null)
+				return;
+
+			CreateCircle();
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+				e.PropertyName == VisualElement.WidthProperty.PropertyName)
+			{
+				CreateCircle();
+			}
+		}
+		private void CreateCircle()
+		{
+			try
+			{
+				double min = Math.Min(Element.Width, Element.Height);
+				Control.Layer.CornerRadius = (float)(min / 2.0);
+				Control.Layer.MasksToBounds = false;
+				Control.Layer.BorderColor = Color.Blue.ToCGColor();
+				Control.Layer.BorderWidth = 1;
+				Control.ClipsToBounds = true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Unable to create circle image: " + ex);
+			}
+		}
+	}
+}
