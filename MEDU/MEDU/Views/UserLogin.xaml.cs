484b4908cebf3e8aa2e2cf36e7f64d6820abc681
DevRabbit <devrabbit@DevRabbits-Mac-mini.local>
2017-08-23 18:46:53 +0530

    Committing change sin UserLogin

--- MEDU/MEDU/Views/UserLogin.xaml.cs
+++ MEDU/MEDU/Views/UserLogin.xaml.cs
@@ -6,115 +6,121 @@ using System.Text.RegularExpressions;
 
 namespace MEDU
 {
-    public partial class UserLogin : ContentPage
-    {
-        string selectedType;
-        //Dictionary<string, string> pickerSource;
-        public double HeightsRequest;
-        public UserLogin()
-        {
-            HeightsRequest = (App.screenHeight * 8) / 100;
-            InitializeComponent();
-            lblOwnerName.Text = Constants.AppOwner;
-            if (Device.OS == TargetPlatform.iOS)
-            {
-                gridBody.HeightRequest = (App.screenHeight * 21) / 100;
-            }
-            else
-            {
-                gridBody.HeightRequest = (App.screenHeight * 28) / 100;
-            }
-            selectedType = "";
-            entryUName.Focused += unameFocused;
-            entryUName.Unfocused += unameUnFocused;
-            //stackUName.HeightRequest = (App.screenHeight * 8) / 100;
-            entryUPassword.Focused += uPasswordFocused;
-            entryUPassword.Unfocused += uPasswordUnFocused;
-            entryUPassword.IsCustomPassword = true;
-            entryUPassword.IsPassword = true;
-            //stackUPassword.HeightRequest = (App.screenHeight * 8) / 100;
-            //pickerUType.Focused += TypeFocused;
-            //pickerUType.Unfocused += TypeUnFocused;
-            //pickerUType.SelectedIndexChanged += SelectedPickerData;
-            ////stackUType.HeightRequest = (App.screenHeight * 8) / 100;
-            //pickerSource = new Dictionary<string, string>();
-            //pickerSource.Add("Student", "Student");
-            //pickerSource.Add("Staff", "Staff");
-            //pickerSource.Add("Admin", "Admin");
-            //foreach (var item in pickerSource)
-            //{
-            //    pickerUType.Items.Add(item.Value);
-            //}
-        }
+	public partial class UserLogin : ContentPage
+	{
+		string selectedType;
+		//Dictionary<string, string> pickerSource;
+		public double HeightsRequest;
+		public UserLogin()
+		{
+			HeightsRequest = (App.screenHeight * 8) / 100;
+			InitializeComponent();
+			lblOwnerName.Text = Constants.AppOwner;
+			if (Device.OS == TargetPlatform.iOS)
+			{
+				gridBody.HeightRequest = (App.screenHeight * 21) / 100;
+			}
+			else
+			{
+				gridBody.HeightRequest = (App.screenHeight * 28) / 100;
+			}
+			selectedType = "";
+			entryUName.Focused += unameFocused;
+			entryUName.Unfocused += unameUnFocused;
+			entryUPassword.Focused += uPasswordFocused;
+			entryUPassword.Unfocused += uPasswordUnFocused;
+			entryUPassword.IsCustomPassword = true;
+			entryUPassword.IsPassword = true;
 
+		}
 
-        void unameFocused(object sender, FocusEventArgs e)
-        {
-            try
-            {
-                lblUName.Opacity = 1;
-                entryUName.Placeholder = "";
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-        }
+		void mobileNoChanged(object sender, TextChangedEventArgs e)
+		{
+			var mobileObject = (Entry)sender;
+			var textValue = mobileObject.Text;
+			if (textValue.Length > 10)
+			{
+				entryUName.Text = textValue.Remove(textValue.Length - 1);
+			}
+		}
 
-        void unameUnFocused(object sender, FocusEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(entryUName.Text))
-                {
-                    lblUName.Opacity = 0;
-                    entryUName.Placeholder = "User Name";
-                }
-                else
-                {
-                    lblUName.Opacity = 1;
-                }
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-        }
+		void passwordChanged(object sender, TextChangedEventArgs e)
+		{
+			var passwordObject = (Entry)sender;
+			var textValue = passwordObject.Text;
+			if (textValue.Length > 4)
+			{
+				entryUPassword.Text = textValue.Remove(textValue.Length - 1);
+			}
+		}
 
-        void uPasswordFocused(object sender, FocusEventArgs e)
-        {
-            try
-            {
-                lblUpasswprd.Opacity = 1;
-                entryUPassword.Placeholder = "";
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-        }
+		void unameFocused(object sender, FocusEventArgs e)
+		{
+			try
+			{
+				lblUName.Opacity = 1;
+				entryUName.Placeholder = "";
+			}
+			catch (Exception ex)
+			{
+				var msg = ex.Message;
+			}
+		}
 
-        void uPasswordUnFocused(object sender, FocusEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(entryUName.Text))
-                {
-                    lblUpasswprd.Opacity = 0;
-                    entryUPassword.Placeholder = "Password";
-                }
-                else
-                {
-                    lblUpasswprd.Opacity = 1;
-                }
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-        }
+		void unameUnFocused(object sender, FocusEventArgs e)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(entryUName.Text))
+				{
+					lblUName.Opacity = 0;
+					entryUName.Placeholder = "User Name";
+				}
+				else
+				{
+					lblUName.Opacity = 1;
+				}
+			}
+			catch (Exception ex)
+			{
+				var msg = ex.Message;
+			}
+		}
 
-        /*
+		void uPasswordFocused(object sender, FocusEventArgs e)
+		{
+			try
+			{
+				lblUpasswprd.Opacity = 1;
+				entryUPassword.Placeholder = "";
+			}
+			catch (Exception ex)
+			{
+				var msg = ex.Message;
+			}
+		}
+
+		void uPasswordUnFocused(object sender, FocusEventArgs e)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(entryUName.Text))
+				{
+					lblUpasswprd.Opacity = 0;
+					entryUPassword.Placeholder = "Password";
+				}
+				else
+				{
+					lblUpasswprd.Opacity = 1;
+				}
+			}
+			catch (Exception ex)
+			{
+				var msg = ex.Message;
+			}
+		}
+
+		/*
         void TypeFocused(object sender, FocusEventArgs e)
         {
             try
@@ -171,79 +177,73 @@ namespace MEDU
         }
         */
 
-        void LoginClicked(object sender, EventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(entryUName.Text))
-                {
-                    DisplayAlert("Alert", "User Name should not be empty", "Ok");
-                }
-                else if (!Regex.IsMatch(entryUName.Text, @"^([1-9])([0-9]*)$"))
-                {
-                    DisplayAlert("Alert", "User Name should Mobile Number", "Ok");
-                }
-                else if (entryUName.Text.Length != 10)
-                {
-                    DisplayAlert("Alert", "This is not a valid User Id", "Ok");
-                }
-                else if (string.IsNullOrEmpty(entryUPassword.Text))
-                {
-                    DisplayAlert("Alert", "User Password should not be empty", "Ok");
-                }
-                else if (entryUPassword.Text.Length != 4)
-                {
+		void LoginClicked(object sender, EventArgs e)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(entryUName.Text))
+				{
+					DisplayAlert("Alert", "User Name should not be empty", "Ok");
+				}
+				else if (!Regex.IsMatch(entryUName.Text, @"^([1-9])([0-9]*)$"))
+				{
+					DisplayAlert("Alert", "User Name should Mobile Number", "Ok");
+				}
+				else if (entryUName.Text.Length < 10)
+				{
+					DisplayAlert("Alert", "Mobile number must be 10 digits", "Ok");
+				}
+				else if (string.IsNullOrEmpty(entryUPassword.Text))
+				{
+					DisplayAlert("Alert", "User Password should not be empty", "Ok");
+				}
+				else if (entryUPassword.Text.Length < 4)
+				{
+					DisplayAlert("Alert", "Password must be 4 digits", "Ok");
+				}
+				else
+				{
+					Navigation.PushModalAsync(new StudentSelection());
+				}
+			}
+			catch (Exception ex)
+			{
+				var msg = ex.Message;
+			}
+		}
 
-                    DisplayAlert("Alert", "User Password is not Valid", "Ok");
-                }
-                //else if (string.IsNullOrEmpty(selectedType))
-                //{
-                //    DisplayAlert("Alert", "User Type should not be empty", "Ok");
-                //}
-                else
-                {
-                    Navigation.PushModalAsync(new StudentSelection());
-                    //App.Current.MainPage = new StudentHomeMaster();
-                }
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-        }
+		void MainNavigationClicked(object sender, EventArgs e)
+		{
+			//try
+			//{
+			//    //if (((Image)sender).Source == ImageSource.FromFile("imgHomeIcon.png"))
+			//    //{
+			//    //    Navigation.PopModalAsync();
+			//    //}
+			//    //else
+			//    //{
+			//    //    var ParentPage = (MasterDetailPage)this.Parent;
+			//    //    ParentPage.IsPresented = (ParentPage.IsPresented == false) ? true : false;
+			//    //}
+			//    if (isFromHome == true)
+			//    {
+			//        Navigation.PopModalAsync();
+			//    }
+			//    else
+			//    {
+			//        var ParentPage = (MasterDetailPage)this.Parent;
+			//        ParentPage.IsPresented = (ParentPage.IsPresented == false) ? true : false;
+			//    }
+			//}
+			//catch (Exception ex)
+			//{
+			//    var msg = ex.Message;
+			//}
+		}
 
-        void MainNavigationClicked(object sender, EventArgs e)
-        {
-            //try
-            //{
-            //    //if (((Image)sender).Source == ImageSource.FromFile("imgHomeIcon.png"))
-            //    //{
-            //    //    Navigation.PopModalAsync();
-            //    //}
-            //    //else
-            //    //{
-            //    //    var ParentPage = (MasterDetailPage)this.Parent;
-            //    //    ParentPage.IsPresented = (ParentPage.IsPresented == false) ? true : false;
-            //    //}
-            //    if (isFromHome == true)
-            //    {
-            //        Navigation.PopModalAsync();
-            //    }
-            //    else
-            //    {
-            //        var ParentPage = (MasterDetailPage)this.Parent;
-            //        ParentPage.IsPresented = (ParentPage.IsPresented == false) ? true : false;
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    var msg = ex.Message;
-            //}
-        }
-
-        //protected override bool OnBackButtonPressed()
-        //{
-        //    return base.OnBackButtonPressed();
-        //}
-    }
+		//protected override bool OnBackButtonPressed()
+		//{
+		//    return base.OnBackButtonPressed();
+		//}
+	}
 }
