4d95edc46aa3973c9f77d28cf29b0d487e8345f8
DevRabbit <devrabbit@DevRabbits-Mac-mini.local>
2017-09-01 13:02:11 +0530

    Committing OTPVerification page

---
+++ MEDU/MEDU/Views/Student/OTPVerification.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MEDU
+{
+	public partial class OTPVerification : ContentPage
+	{
+		//CustomProperties cp = new CustomProperties();
+		public string _mobileNo;
+		public OTPVerification()
+		{
+
+			InitializeComponent();
+		}
+		private async void BtnVerify_Click(object sender, EventArgs args)
+		{
+
+
+		}
+
+		public async void GetVerificationCode()
+		{
+
+
+		}
+
+		string str;
+
+		public async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+		{
+
+
+
+		}
+
+		public async void GetResendVerificationCode()
+		{
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+
+				var msg = ex.Message;
+			}
+		}
+
+		public async void BackImageTapped(object sender, EventArgs args)
+		{
+
+		}
+
+	}
+}
+
+
+
