ce6436555edbc12e4b48886ee44eb149a15534ad
DevRabbit <devrabbit@DevRabbits-Mac-mini.local>
2017-09-01 14:47:01 +0530

    Committing Frame Renderer

---
+++ MEDU/Droid/CustomControls/MyFrameRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using Android.Text;
+using Android.Util;
+using MEDU;
+using MEDU.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//using System.ComponentModel;
+//using System.Diagnostics;
+//using System.Drawing;
+
+[assembly: ExportRenderer(typeof(MyFrame), typeof(MyFrameRenderer))]
+namespace MEDU.Droid
+{
+	public class MyFrameRenderer : FrameRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+		{
+			base.OnElementChanged(e);
+#pragma warning disable CS0618
+			this.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.blue_rect));
+#pragma warning restore CS0618
+		}
+	}
+}
+
---
+++ MEDU/iOS/CustomControls/MyFrameRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MEDU;
+using MEDU.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+using CoreGraphics;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(MyFrame), typeof(MyFrameRenderer))]
+
+namespace MEDU.iOS
+{
+	public class MyFrameRenderer : FrameRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+		{
+			base.OnElementChanged(e);
+			this.Layer.CornerRadius = 18;
+			this.Layer.Bounds.Inset(1, 1);
+			Layer.BorderColor = UIColor.White.CGColor;
+			Layer.BorderWidth = 2;
+			Layer.BackgroundColor = Color.Transparent.ToCGColor();
+		}
+	}
+}
